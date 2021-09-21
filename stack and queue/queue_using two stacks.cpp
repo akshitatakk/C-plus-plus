007f92a063bd392dc3ef8687192f35e12ed8b697
akshitatakk <58398486+akshitatakk@users.noreply.github.com>
2021-09-21 22:50:46 +0530

    Add files via upload

---
+++ stack and queue/queue_using two stacks.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class que{
+	stack<int> s1;
+	stack<int> s2;
+	
+	public:
+		void push(int x){
+			s1.push(x);
+		}
+		
+		int pop(){
+			if(s1.empty() && s2.empty()){
+				cout<<"empty"<<endl;
+				return -1;
+			}
+			if(s2.empty()){
+				while(!s1.empty()){
+					s2.push(s1.top());
+					s1.pop();
+				}
+				int topval =s2.top();
+				s2.pop();
+				
+				while(!s2.empty()){
+                s1.push(s2.top());
+                s2.pop();
+                }
+				return topval;
+			}
+		}
+		
+		bool empty(){
+			if(s1.empty() && s2.empty()){
+				return true;
+			}
+			return false;
+		}
+};
+
+int main(){
+    que q;
+    q.push(1);
+    q.push(2);
+    cout<<q.pop()<<endl;
+    cout<<q.pop()<<endl;
+    cout<<q.empty()<<endl;
+	return 0;
+}
